38c67f0339e0fb96ce1af1f72725cf0a7cc3a849
Oscar Falkman <ofalkman@gmail.com>
2017-12-29 11:21:30 +0100

    Added JSF and EJB

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -5,6 +5,6 @@ import net.EchoEndpoint;
 public class View {
     public static void main(String[] args) {
         EchoEndpoint endpoint = new EchoEndpoint();
-        endpoint.open();
+        //endpoint.open();
     }
 }
